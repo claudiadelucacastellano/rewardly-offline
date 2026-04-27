c392d6292148d5fd5746f6917c0c242f6737275b
Claudia <claudia.delucacastellano@gmail.com>
2026-04-27 10:16:46 +0200

    No blank page

---
+++ app/types.d.ts
@@ -0,0 +1,16 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    "s-app-nav": any;
+    "s-link": any;
+    "s-page": any;
+    "s-section": any;
+    "s-button": any;
+    "s-paragraph": any;
+    "s-stack": any;
+    "s-box": any;
+    "s-heading": any;
+    "s-text": any;
+    "s-unordered-list": any;
+    "s-list-item": any;
+  }
+}
